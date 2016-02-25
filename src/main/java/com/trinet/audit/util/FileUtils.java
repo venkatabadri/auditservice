42afd93cd84e3661905567b0a8c968c9a3782623
praveen_neelam <praveen_neelam@HSD23176.persistent.co.in>
2016-02-25 17:08:08 +0530

    Implemented SONAR changes.

--- src/main/java/com/trinet/audit/util/FileUtils.java
+++ src/main/java/com/trinet/audit/util/FileUtils.java
@@ -14,25 +14,30 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class FileUtils {
-    
+
     /* Instance of Logger Factory */
     private static final Logger LOGGER = LoggerFactory.getLogger(FileUtils.class);
 
-   /**
-    * Writing audit data to the specified location
-    * If location not exists it will create directory
-    * @param location
-    * @param auditdata
-    */
-    public static void writeToFile(String location, String auditdata){
-       
+    private FileUtils() {
+        super();
+    }
+
+    /**
+     * Writing audit data to the specified location If location not exists it
+     * will create directory
+     * 
+     * @param location
+     * @param auditdata
+     */
+    public static void writeToFile(String location, String auditdata) {
+
         File directory = new File(location);
-        if (! directory.exists()){
+        if (!directory.exists()) {
             directory.mkdir();
         }
         try {
-            File auditFile = new File (directory,"auditlog.txt");
-            FileWriter fw = new FileWriter(auditFile,true);
+            File auditFile = new File(directory, "auditlog.txt");
+            FileWriter fw = new FileWriter(auditFile, true);
             fw.write(auditdata + "\n");
             fw.close();
         } catch (Exception ex) {
--- src/main/java/com/trinet/audit/util/ServiceConstants.java
+++ src/main/java/com/trinet/audit/util/ServiceConstants.java
@@ -6,13 +6,32 @@ package com.trinet.audit.util;
  */
 public final class ServiceConstants {
 
+    /**
+     * response Success
+     */
     public static final String MESSAGE_RESPONSE_SUCCESS = "Success";
 
+    /**
+     * response Fail
+     */
     public static final String MESSAGE_RESPONSE_FAIL = "Fail";
 
+    /**
+     * File appender type is mongodb
+     */
     public static final String STORAGE_TYPE_MONGO = "mongodb";
 
+    /**
+     * File appender type is file
+     */
     public static final String STORAGE_TYPE_FLATFILE = "file";
 
+    /**
+     * Audit filename
+     */
     public static final String AUDIT_FILENAME = "auditlog.txt";
+
+    private ServiceConstants() {
+        super();
+    }
 }
